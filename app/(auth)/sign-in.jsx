254e3659bb910e157518acdda9254477ee18d1a4
Mashi-91 <aadilhasan9697@email.com>
2024-12-28 22:05:40 +0530

    updated

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,49 @@
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import images from "../../constants/images";
+import FormField from "../../components/FormField";
+
+const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  return (
+    <>
+      <SafeAreaView className="bg-primary h-full">
+        <ScrollView>
+          <View className="w-full justify-center h-full px-4 my-6">
+            <Image
+              source={images.logo}
+              resizeMode="contain"
+              className="w-[115px] h-[35px]"
+            />
+            <Text className="text-2xl text-white font-psemibold text-semibold mt-10">
+              Login In to Aora
+            </Text>
+
+            <FormField
+              title="Email"
+              value={form.email}
+              handleChangeText={(e) => setForm({ ...form, email: e })}
+              otherStyle="mt-7"
+              keyboardType="email-address"
+            />
+
+            <FormField
+              title="Password"
+              value={form.password}
+              handleChangeText={(e) => setForm({ ...form, password: e })}
+              otherStyle="mt-7"
+            />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default SignIn;
---
+++ components/FormField.jsx
@@ -0,0 +1,22 @@
+import { View, Text, TextInput } from "react-native";
+import React from "react";
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyle,
+  ...props
+}) => {
+  return (
+    <View className={`space-y-2 ${otherStyle}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 items-center rounded-2xl focus:border-secondary"> 
+        
+        </View>
+    </View>
+  );
+};
+
+export default FormField;
